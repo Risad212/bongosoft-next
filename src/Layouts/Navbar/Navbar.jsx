aee5f857313d35ba9fe00270696e74b5c267ef24
Risad212 <hafez.risad@gmail.com>
2023-04-03 13:00:57 +0600

    'done'

--- src/Layouts/Navbar/Navbar.jsx
+++ src/Layouts/Navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import navbar from './navbar.module.css';
 import Logo from '../../Media/logo.png'
 import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 const Navbar = () => {
@@ -9,19 +11,80 @@ const Navbar = () => {
         <>
           <div className={navbar.navWrapper}>
              <div className="container">
+                <nav>
                 {/*----- LOGO ------*/}
                  <div className={navbar.logo}>
                     <a href="#"><Image src={Logo} alt="" /></a>
                  </div>
                  <div className={navbar.menuWrapper}>
-                     <ul>
-                        <li><a href="#"></a>Home</li>
-                        <li><a href="#"></a>Home</li>
-                        <li><a href="#"></a>Home</li>
-                        <li><a href="#"></a>Home</li>
-                        <li><a href="#"></a>Home</li>
+                     <ul className={navbar.list}>
+                        <li><a href="#" className={navbar.active}>Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li className={navbar.dropdown}>
+                           <a href="#">Services</a>
+                           <ul class={navbar.dlSubmenu}>
+                              <li> <a href="software-development.php">	Software Development </a>
+                                  <ul class="secondary dl-submenu">
+                                      <li> <a href="#"> Point Of Sale </a> </li>
+                                      <li> <a href="#"> School Management </a> </li>
+                                      <li> <a href="#"> Pharmacy Management </a> </li>
+                                      <li> <a href="#"> Super Shop Management </a> </li>
+                                      <li> <a href="#"> Restaurant Management </a> </li>
+                                      <li> <a href="#"> Custom Software </a> </li>
+                                  </ul>
+                              </li>
+                              <li> <a href="web-design.php"> Web Design </a>
+                                  <ul class="secondary dl-submenu">
+                                      <li> <a href="#" title=""> PSD to HTML </a> </li>
+                                      <li> <a href="#" title=""> UX/UI Design </a> </li>
+                                      <li> <a href="#" title=""> Website Template Design </a> </li>
+                                      <li> <a href="#" title=""> PSD/HTML to WordPress </a> </li>
+                                      <li> <a href="#" title=""> Front End Design </a> </li>
+                                      <li> <a href="#" title=""> Email Template </a> </li>
+                                  </ul>
+                              </li>
+                              <li> <a href="web-development.php">	Web Development </a>
+                                  <ul class="secondary dl-submenu">
+                                      <li> <a href="#"> News Portal development </a> </li>
+                                      <li> <a href="#"> Custom CMS development </a> </li>
+                                      <li> <a href="#"> E-Commerce Development </a> </li>
+                                      <li> <a href="#"> NGO Website Development </a> </li>
+                                      <li> <a href="#"> Educational Ins. Website </a> </li>
+                                      <li> <a href="#"> WordPress Development</a> </li>
+                                  </ul>
+                              </li>
+                              <li> <a href="graphics-design.php">	Graphics Design </a>
+                                  <ul class="secondary dl-submenu">
+                                      <li> <a href="#"> Logo Design </a> </li>
+                                      <li> <a href="#"> Flyer &amp; Brochure Design </a> </li>
+                                      <li> <a href="#"> Corporate Branding </a> </li>
+                                      <li> <a href="#"> PSD Template Design </a> </li>
+                                      <li> <a href="#"> Web &amp; Social Media Ad. </a> </li>
+                                      <li> <a href="#"> Prient Media Design </a> </li>
+                                  </ul>
+                              </li>
+                              <li> <a href="seo-smm.php">	Digital Marketing </a>
+                                  <ul class="secondary dl-submenu">
+                                      <li><a href="#"> Social Media Ad. Campaign </a></li>
+                                      <li><a href="#"> Onsite SEO Service </a></li>
+                                      <li><a href="#"> Website Ranking Service </a></li>
+                                      <li><a href="#"> E-Commerce SEO Solution </a></li>
+                                      <li><a href="#"> Corporate SEO Service </a></li>
+                                      <li><a href="#"> Local SEO Service</a></li>
+                                  </ul>
+                              </li>
+                              <li> <a href="#"> Domain &amp; Hosting </a> </li>
+                          </ul>
+                        </li>
+                        <li><a href="#">our work</a></li>
+                        <li><a href="#">blog</a></li>
+                        <li><a href="#">contact</a></li>
                      </ul>
-                 </div>
+                     <div className={navbar.searchBtn}>
+                        <FontAwesomeIcon icon={faSearch}/>
+                     </div>
+                    </div>
+                 </nav>
              </div>
           </div>
         </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,19 @@
-import Layout from '@/Layouts/Layout'
-import styles from '@/styles/Home.module.css'
-import { Html } from 'next/document'
 import Head from 'next/head'
 
 export default function Home() {
   return (
-     <>
-     </>
+    <>
+      <Head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Bongosoft Ltd. | Software Company in Bangladesh </title>
+        <meta name="description" content="Bongosoft is a leading web design company in Bangladesh. We are also a digital marketing agency in Bangladesh providing not only website design &amp; development, seo service but also graphic designing, content marketing, making online Bangla newspaper and effective social media marketing all over Bangladesh and many more." />
+        {/*-- google font --- */}
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@400;600;700&display=swap" rel="stylesheet"></link> */}
+      </Head>
+    </>
   )
 }
