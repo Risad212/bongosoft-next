e02d05c8eff9d4f6e3403f89307dfdb846b5f165
Risad212 <hafez.risad@gmail.com>
2023-05-21 18:42:29 +0600

    done

--- src/Layouts/service/Service.jsx
+++ src/Layouts/service/Service.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import service from './service.module.css';
 import Image from 'next/image';
-import softoware from '../../Media/software.png';
+// ========= Icon import =======================
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowDownLong, faArrowRight, faArrowRightArrowLeft, faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+// ========= Image import =======================
+import softoware from '../../Media/software.png';
+import webDesign from '../../Media/design.png';
+import webdev from '../../Media/marketing-bg-1.jpg'
+
 
 const Service = () => {
+    console.log(softoware);
     return (
         <>
             <section className={service.service_2}>
@@ -22,119 +28,65 @@ const Service = () => {
                         </div>
                     </div>
                     <div className={service.service_grid}>
-                        <div classNameName={`${service.service_item} software`}>
-                            <div className={service.service_details}>
-                                <div className={service.service_content}>
-                                    <div className="icon">
-                                         <Image className={service.web_development} src="images/custom/service-section/software.png" alt="Software Development" />
-                                     </div>
+                        <div class={`${service.service_item} ${service.software}`}>
+                            <div class={service.service_details}>
+                                <div class={service.service_content}>
+                                    <div>
+                                        <Image src={softoware} alt="Software Development" />
+                                    </div>
                                     <h3>Software Development</h3>
-                                    <ul className={service.service_sub_title}>
-                                        <li><FontAwesomeIcon icon={faArrowRightLong}/><i className="fa fa-long-arrow-right"></i> Point Of Sale </li>
-                                        <li> <FontAwesomeIcon icon={faArrowRightLong}/><i className="fa fa-long-arrow-right"></i> School Management </li>
-                                        <li><FontAwesomeIcon icon={faArrowRightLong}/><i className="fa fa-long-arrow-right"></i> Pharmacy Management </li>
-                                        <li><FontAwesomeIcon icon={faArrowRightLong}/><i className="fa fa-long-arrow-right"></i> Super Shop Management</li>
-                                        <li><FontAwesomeIcon icon={faArrowRightLong}/><i className="fa fa-long-arrow-right"></i> Restaurant Management </li>
-                                        <li><FontAwesomeIcon icon={faArrowRightLong}/><i className="fa fa-long-arrow-right"></i> Custom Software </li>
+                                    <ul class={service.service_sub_title}>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Point Of Sale </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> School Management </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Pharmacy Management </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Super Shop Management</li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Restaurant Management </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Custom Software </li>
                                     </ul>
-                                    <a className={`btn ${service.btn_service}`} href="#">Details</a>
+                                    <a class={`btn ${service.btn_service}`} href="software-development.php">Details</a>
                                 </div>
                             </div>
                         </div>
-
-                        {/* <div className="service_item web-design wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".3s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 0.3s; animation-name: fadeInUp;">
-                            <div className="service_details">
-                                <div className="service_content">
-                                    <div className="icon"><img className="web_design_animation" src="images/custom/service-section/design.png" alt="Web Design"></div>
+                        {/*----- service item --------*/}
+                        <div class={`${service.service_item} ${service.web_design}`}>
+                            <div class={service.service_details}>
+                                <div class={service.service_content}>
+                                    <div>
+                                       <Image src={webDesign} alt="Web Design" />
+                                     </div>
                                     <h3>Web Design</h3>
-                                    <ul className="service_sub_title">
-                                        <li><i className="fa fa-long-arrow-right"></i> PSD To HTML </li>
-                                        <li> <i className="fa fa-long-arrow-right"></i> UI/UX Design </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Web Template Design </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> PSD/HTML To WordPress</li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Front End Design </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Email Template </li>
-                                    </ul><!--/.service_sub_title-->
-                                    <a className="btn btn-service" href="web-design.php">Details</a>
-                                </div><!--/.service_content-->
-                            </div><!--/.service_details-->
-                        </div> */}
-
-                        {/* <div className="service_item web-development wow fadeInDown" data-wow-duration=".5s" data-wow-delay=".6s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 0.6s; animation-name: fadeInDown;">
-                            <div className="service_details">
-                                <div className="service_content">
-                                    <div className="icon"><img className="web_development_animation" src="images/custom/service-section/development.png" alt="Web Development"></div>
+                                    <ul class={service.service_sub_title}>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> PSD To HTML </li>
+                                        <li> <FontAwesomeIcon icon={faArrowRightLong} /> UI/UX Design </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Web Template Design </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> PSD/HTML To WordPress</li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Front End Design </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Email Template </li>
+                                    </ul>
+                                    <a class={`btn ${service.btn_service}`} href="web-design.php">Details</a>
+                                </div>
+                            </div>
+                        </div>
+                         {/*----- service item --------*/}
+                         <div class={`${service.service_item} ${service.web_development}`}>
+                            <div class={service.service_details}>
+                                <div class={service.service_content}>
+                                    <div>
+                                       <Image src={webdev} alt="Web Development" />
+                                     </div>
                                     <h3>Web Development</h3>
-                                    <ul className="service_sub_title">
-                                        <li><i className="fa fa-long-arrow-right"></i> News Portal Development </li>
-                                        <li> <i className="fa fa-long-arrow-right"></i> Custom CMS Development </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Educational Ins. Website </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> E-Commerce Development</li>
-                                        <li><i className="fa fa-long-arrow-right"></i> NGO Website Development </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> WordPress Development </li>
-                                    </ul><!--/.service_sub_title-->
-                                    <a className="btn btn-service" href="web-development.php">Details</a>
-                                </div><!--/.service_content-->
-                            </div><!--/.service_details-->
-                        </div>--/.service_item-- */}
-
-                        <div className="service_item graphic wow fadeInRight" data-wow-duration=".5s" data-wow-delay="1.8s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 1.8s; animation-name: fadeInRight;">
-                            <div className="service_details">
-                                <div className="service_content">
-                                    <div className="icon"><img className="web_development" src="images/custom/service-section/graphic.png" alt="Graphic Design"></div>
-                                    <h3>Graphic Design</h3>
-                                    <ul className="service_sub_title">
-                                        <li><i className="fa fa-long-arrow-right"></i> Logo Design </li>
-                                        <li> <i className="fa fa-long-arrow-right"></i> Flyer &amp; Brochure Design </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Corporate Branding </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> PSD Template Design </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Web &amp; Social Media Ad. </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Prient Media Design </li>
-                                    </ul><!--/.service_sub_title-->
-                                    <a className="btn btn-service" href="graphics-design.php">Details</a>
-                                </div><!--/.service_content-->
-                            </div><!--/.service_details-->
-                        </div>{/* --/.service_item-- */}
-
-                        <div className="service_item seo wow fadeInLeft" data-wow-duration=".5s" data-wow-delay="1.5s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 1.5s; animation-name: fadeInLeft;">
-                            <div className="service_details">
-                                <div className="service_content">
-                                    <div className="icon"><img className="web_development_animation" src="images/custom/service-section/seo.png" alt="SEO &amp; SMM"></div>
-                                    <h3>Digital Marketing</h3>
-                                    <ul className="service_sub_title">
-                                        <li><i className="fa fa-long-arrow-right"></i> Social Media Ad. Campaign </li>
-                                        <li> <i className="fa fa-long-arrow-right"></i> Onsite SEO Service </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Website Ranking Service </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> PSD Template Design </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Web &amp; Social Media Ad. </li>
-                                        <li><i className="fa fa-long-arrow-right"></i> Prient Media Design </li>
-                                    </ul><!--/.service_sub_title-->
-                                    <a className="btn btn-service" href="seo-smm.php">Details</a>
-                                </div><!--/.service_content-->
-                            </div><!--/.service_details-->
-                        </div>{/* --/.service_item-- */}
-
-                        <div className="service_item domain wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".9s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 0.9s; animation-name: fadeInUp;">
-                            <div className="service_details">
-                                <a href="#">
-                                    <div className="service_content">
-                                        <div className="icon"><img className="web_domain" src="images/custom/service-section/domain.png" alt="Domain &amp; Hosting"></div>
-                                        <h3>Domain &amp; Hosting</h3>
-                                    </div><!--/.service_content-->
-                                </a>
-                            </div><!--/.service_details-->
-                        </div>{/* --/.service_item-- */}
-
-                        <div className="service_item support wow fadeInRight" data-wow-duration=".5s" data-wow-delay="2.1s" style="visibility: visible; animation-duration: 0.5s; animation-delay: 2.1s; animation-name: fadeInRight;">
-                            <div className="service_details">
-                                <a href="#">
-                                    <div className="service_content">
-                                        <div className="icon"><img className="bg-red" src="images/custom/service-section/suppport.png" alt="Support"></div>
-                                        <h3>Support</h3>
-                                    </div><!--/.service_content-->
-                                </a>
-                            </div><!--/.service_details-->
-                        </div>{/* --/.service_item-- */}
+                                    <ul class={service.service_sub_title}>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> News Portal Development </li>
+                                        <li> <FontAwesomeIcon icon={faArrowRightLong} /> Custom CMS Development</li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> Educational Ins. Website </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> E-Commerce Development </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> NGO Website Development </li>
+                                        <li><FontAwesomeIcon icon={faArrowRightLong} /> WordPress Development </li>
+                                    </ul>
+                                    <a class={`btn ${service.btn_service}`} href="web-design.php">Details</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
