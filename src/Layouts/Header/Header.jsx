067b0c39f13b90302da88480c5effa0d241145b9
Risad212 <hafez.risad@gmail.com>
2023-05-23 16:16:37 +0600

    done

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import header from './header.module.css';
 import Topbar from '../Topbar/Topbar';
 import Navbar from '../Navbar/Navbar';
 
--- src/Layouts/Slider/Slider.jsx
+++ src/Layouts/Slider/Slider.jsx
@@ -1,8 +1,8 @@
-import React,{useRef} from "react";
+import React from "react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Swiper, SwiperSlide } from "swiper/react";
-import {Navigation,Pagination } from "swiper";
+import { Navigation, Pagination } from "swiper";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 // =============== My Import  ====================
@@ -11,23 +11,26 @@ import slider1 from '../../Media/slider1.jpg'
 import slider2 from '../../Media/slider2.jpg'
 import slider3 from '../../Media/slider3.jpg'
 import slider4 from '../../Media/slider4.jpg'
+import slider from './slider.module.css';
 
 const Slider = () => {
   return (
     <>
-      <Swiper 
-        navigation={true}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Navigation,Pagination]} 
-        loop={true}
-        className="mySwiper">
-        <SwiperSlide><Image src={slider1}/></SwiperSlide>
-        <SwiperSlide><Image src={slider2}/></SwiperSlide>
-        <SwiperSlide><Image src={slider3}/></SwiperSlide>
-        <SwiperSlide><Image src={slider4}/></SwiperSlide>
-      </Swiper>
+      <div>
+        <Swiper
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination]}
+          loop={true}
+          className={slider.mySwiper}>
+          <SwiperSlide><Image src={slider1} /></SwiperSlide>
+          <SwiperSlide><Image src={slider2} /></SwiperSlide>
+          <SwiperSlide><Image src={slider3} /></SwiperSlide>
+          <SwiperSlide><Image src={slider4} /></SwiperSlide>
+        </Swiper>
+      </div>
     </>
   )
 };
