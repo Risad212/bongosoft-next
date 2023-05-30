8df63800aeec417c3746999c037d44e5caaa3d28
Risad212 <hafez.risad@gmail.com>
2023-05-30 17:39:22 +0600

    done

--- src/Layouts/blog/Blog.jsx
+++ src/Layouts/blog/Blog.jsx
@@ -17,10 +17,10 @@ const Blog = () => {
                             <figure class={blog.image}><Image class="img-fluid" src={chrome} alt="Top 10 Productivity Extensions for Chrome Web Browser" /></figure>
                             <div class={blog.content_box}>
                                 <div class={blog.text_content}>
-                                    <h2><a href="chrome-top-ten-extensions-details.php">Top 10 Productivity Extensions for Chrome Web Browser</a></h2>
+                                    <h2><a href="#">Top 10 Productivity Extensions for Chrome Web Browser</a></h2>
                                     <div class={blog.info}>By <em><a href="#">Admin</a></em> at October 28, 2019 </div>
                                     <p class={blog.text}>There are limitless ways to ...</p>
-                                    <div class="link-btn"><a href="chrome-top-ten-extensions-details.php" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong}/></span></a></div>
+                                    <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong}/></span></a></div>
                                 </div>
                             </div>
                         </article>
