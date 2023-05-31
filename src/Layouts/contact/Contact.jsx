f78fdb96472c8edf7d8c8479d934a0c76507e38e
Risad212 <hafez.risad@gmail.com>
2023-05-31 11:59:52 +0600

    done

--- src/Layouts/contact/Contact.jsx
+++ src/Layouts/contact/Contact.jsx
@@ -19,13 +19,13 @@ const Contact = () => {
                                     <form id="#" action="#">
                                         <div class="row">
                                             <div class="form-group col-6">
-                                                <input placeholder="Your Name" class={`${contact.form_control}` }/>
+                                                <input placeholder="Your Name" class={`${contact.form_control}`} />
                                             </div>
                                             <div class="form-group col-6">
-                                                <input placeholder="Your Email" class={`${contact.form_control}` } />
+                                                <input placeholder="Your Email" class={`${contact.form_control}`} />
                                             </div>
                                             <div class="form-group col-12">
-                                                <input placeholder="Subject" class={`${contact.form_control}` } />
+                                                <input placeholder="Subject" class={`${contact.form_control}`} />
                                             </div>
                                             <div class="form-group col-12">
                                                 <textarea rows="6" placeholder="Message" class={`${contact.form_control}`} name="Message"></textarea>
@@ -40,6 +40,18 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
+            </section>
+            {/*---- location title ------*/}
+            <section class={contact.location_wrapper}>
+                <div class="container">
+                    <div class={contact.wrapper_text_location}>
+                        <h2 class="text-center">Locations</h2>
+                    </div>
+                </div>
+            </section>
+            {/*---- Google map ------*/}
+            <section class={contact.map}>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1290.8991623422357!2d90.42607176278011!3d23.775385352230952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c791e9b32d9b%3A0x2bde4403d24db436!2sBongosoft+Ltd.!5e0!3m2!1sbn!2sbd!4v1519553583950" style={{width: "100%", height:"100%"}} frameborder="0" allowfullscreen=""></iframe>
             </section>
         </>
     );
