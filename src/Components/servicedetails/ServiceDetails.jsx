f04d756abce1bef59d7702f3ff90171c4191000a
Risad212 <hafez.risad@gmail.com>
2023-06-09 17:09:45 +0600

    done

---
+++ src/Components/servicedetails/ServiceDetails.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import layoute from './serviceDetails.module.css';
+import Image from 'next/image';
+import details from '../../Media/service details/software-development.jpg';
+
+
+const ServiceDetails = () => {
+    return (
+        <div>
+            <section className={layoute.service_details_section}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className={layoute.service_image}>
+                                <Image className="img-fluid" src={details} alt="Software Development" />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className={layoute.service_details}>
+                                <h3>Software Development</h3>
+                                <p>Your business needs software for supporting your operations. It requires for scaling and coordinating with all operation department. Bongosoft Ltd. has the experience, tools, and technologies to create such demanding software. You will find some software that we are developing.</p>
+                                <p>We are using asp.net, PHP, Java, Node.js, latest java framework for developing our software. Your need will always get priority from our end. We provide you the best solution in the market. Bongosoft Ltd. has the experience, tools, and technologies to create such demanding software. You will find some software that we are developing.</p>
+                            </div>
+                            <div className={layoute.service_section_btn}>
+                                <a href="#" className="btn-11">Get a quote</a>
+                                <a className="btn-6" href="#">Contact Us Now<span></span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default ServiceDetails;
---
+++ src/pages/service/[name].js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+import ServiceDetails from '../../Components/servicedetails/ServiceDetails';
+
+const ServiceLayout = () => {
+    return (
+        <>
+            <Head>
+                <meta charset="utf-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Bongosoft Ltd. | Services</title>
+                <link rel="shortcut icon" href="images/favicon.png" type="image/png" />
+            </Head>
+            <ServiceDetails />
+        </>
+    );
+};
+
+export default ServiceLayout;
