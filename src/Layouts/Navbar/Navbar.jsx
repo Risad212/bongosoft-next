6c00cd40a9be716c1060b7cea8d0baf5de2aa516
Risad212 <hafez.risad@gmail.com>
2023-07-01 20:49:52 +0600

    done

--- src/Layouts/Navbar/Navbar.jsx
+++ src/Layouts/Navbar/Navbar.jsx
@@ -2,8 +2,9 @@ import React, { useEffect, useState } from 'react';
 import navbar from './navbar.module.css';
 import Logo from '../../Media/logo.png'
 import Image from 'next/image';
+import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faMagnifyingGlass, faSearch, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faMagnifyingGlass,faXmark } from '@fortawesome/free-solid-svg-icons';
 import { useRouter } from 'next/router';
 
 
@@ -39,76 +40,76 @@ const Navbar = () => {
                <nav>
                   {/*----- LOGO ------*/}
                   <div className={navbar.logo}>
-                     <a href="#"><Image src={Logo} alt="" /></a>
+                     <Link href="#"><Image src={Logo} alt="" /></Link>
                   </div>
                   <div className={`${navbar.menuWrapper} ${showMenu? navbar.showMenu: ''}`}>
                      <ul className={navbar.list}>
-                        <li><a href="/" className={currentRoute === '/' ? navbar.active : navbar.nonActive}>Home</a></li>
-                        <li><a href="/about" className={currentRoute === '/about' ? navbar.active : navbar.nonActive}>About</a></li>
+                        <li><Link href="/" className={currentRoute === '/' ? navbar.active : navbar.nonActive}>Home</Link></li>
+                        <li><Link href="/about" className={currentRoute === '/about' ? navbar.active : navbar.nonActive}>About</Link></li>
                         <li className={navbar.dropdown}>
-                           <a href="/service" className={currentRoute === '/service' ? navbar.active : navbar.nonActive}>Services</a>
+                           <Link href="/service" className={currentRoute === '/service' ? navbar.active : navbar.nonActive}>Services</Link>
                            <ul className={navbar.dlSubmenu}>
                               <li className={navbar.firsDrop}>
-                                 <a href="/service/softoware_development">Software Development </a>
+                                 <Link href="/service/softoware_development">Software Development </Link>
                                  <ul id={navbar.secondary} className={navbar.dlSubmenu}>
-                                    <li> <a href="#"> Point Of Sale </a> </li>
-                                    <li> <a href="#"> School Management </a> </li>
-                                    <li> <a href="#"> Pharmacy Management </a> </li>
-                                    <li> <a href="#"> Super Shop Management </a> </li>
-                                    <li> <a href="#"> Restaurant Management </a> </li>
-                                    <li> <a href="#"> Custom Software </a> </li>
+                                    <li> <Link href="#"> Point Of Sale </Link> </li>
+                                    <li> <Link href="#"> School Management </Link> </li>
+                                    <li> <Link href="#"> Pharmacy Management </Link> </li>
+                                    <li> <Link href="#"> Super Shop Management </Link> </li>
+                                    <li> <Link href="#"> Restaurant Management </Link> </li>
+                                    <li> <Link href="#"> Custom Software </Link> </li>
                                  </ul>
                               </li>
                               <li className={navbar.secondDrop}>
-                                 <a href="/service/web_design">Web Design</a>
+                                 <Link href="/service/web_design">Web Design</Link>
                                  <ul id={navbar.secondary} className={navbar.dlSubmenu}>
-                                    <li> <a href="#" title=""> PSD to HTML </a> </li>
-                                    <li> <a href="#" title=""> UX/UI Design </a> </li>
-                                    <li> <a href="#" title=""> Website Template Design </a> </li>
-                                    <li> <a href="#" title=""> PSD/HTML to WordPress </a> </li>
-                                    <li> <a href="#" title=""> Front End Design </a> </li>
-                                    <li> <a href="#" title=""> Email Template </a> </li>
+                                    <li> <Link href="#" title=""> PSD to HTML </Link> </li>
+                                    <li> <Link href="#" title=""> UX/UI Design </Link> </li>
+                                    <li> <Link href="#" title=""> Website Template Design </Link> </li>
+                                    <li> <Link href="#" title=""> PSD/HTML to WordPress </Link> </li>
+                                    <li> <Link href="#" title=""> Front End Design </Link> </li>
+                                    <li> <Link href="#" title=""> Email Template </Link> </li>
                                  </ul>
                               </li>
                               <li className={navbar.thirtDrop}>
-                                 <a href="/service/web_development">Web Development</a>
+                                 <Link href="/service/web_development">Web Development</Link>
                                  <ul id={navbar.secondary} className={navbar.dlSubmenu}>
-                                    <li> <a href="#"> News Portal development </a> </li>
-                                    <li> <a href="#"> Custom CMS development </a> </li>
-                                    <li> <a href="#"> E-Commerce Development </a> </li>
-                                    <li> <a href="#"> NGO Website Development </a> </li>
-                                    <li> <a href="#"> Educational Ins. Website </a> </li>
-                                    <li> <a href="#"> WordPress Development</a> </li>
+                                    <li> <Link href="#"> News Portal development </Link> </li>
+                                    <li> <Link href="#"> Custom CMS development </Link> </li>
+                                    <li> <Link href="#"> E-Commerce Development </Link> </li>
+                                    <li> <Link href="#"> NGO Website Development </Link> </li>
+                                    <li> <Link href="#"> Educational Ins. Website </Link> </li>
+                                    <li> <Link href="#"> WordPress Development</Link> </li>
                                  </ul>
                               </li>
                               <li className={navbar.fourDrop}>
-                                 <a href="/service/graphics_design">Graphics Design </a>
+                                 <Link href="/service/graphics_design">Graphics Design </Link>
                                  <ul id={navbar.secondary} className={navbar.dlSubmenu}>
-                                    <li> <a href="#"> Logo Design </a> </li>
-                                    <li> <a href="#"> Flyer &amp; Brochure Design </a> </li>
-                                    <li> <a href="#"> Corporate Branding </a> </li>
-                                    <li> <a href="#"> PSD Template Design </a> </li>
-                                    <li> <a href="#"> Web &amp; Social Media Ad. </a> </li>
-                                    <li> <a href="#"> Prient Media Design </a> </li>
+                                    <li> <Link href="#"> Logo Design </Link> </li>
+                                    <li> <Link href="#"> Flyer &amp; Brochure Design </Link> </li>
+                                    <li> <Link href="#"> Corporate Branding </Link> </li>
+                                    <li> <Link href="#"> PSD Template Design </Link> </li>
+                                    <li> <Link href="#"> Web &amp; Social Media Ad. </Link> </li>
+                                    <li> <Link href="#"> Prient Media Design </Link> </li>
                                  </ul>
                               </li>
                               <li className={navbar.fifthDrop}>
-                                 <a href="/service/seo_smm">Digital Marketing </a>
+                                 <Link href="/service/seo_smm">Digital Marketing </Link>
                                  <ul id={navbar.secondary} className={navbar.dlSubmenu}>
-                                      <li><a href="#"> Social Media Ad. Campaign </a></li>
-                                      <li><a href="#"> Onsite SEO Service </a></li>
-                                      <li><a href="#"> Website Ranking Service </a></li>
-                                      <li><a href="#"> E-Commerce SEO Solution </a></li>
-                                      <li><a href="#"> Corporate SEO Service </a></li>
-                                      <li><a href="#"> Local SEO Service</a></li>
+                                      <li><Link href="#"> Social Media Ad. Campaign </Link></li>
+                                      <li><Link href="#"> Onsite SEO Service </Link></li>
+                                      <li><Link href="#"> Website Ranking Service </Link></li>
+                                      <li><Link href="#"> E-Commerce SEO Solution </Link></li>
+                                      <li><Link href="#"> Corporate SEO Service </Link></li>
+                                      <li><Link href="#"> Local SEO Service</Link></li>
                                   </ul>
                               </li>
-                              <li> <a href="#"> Domain &amp; Hosting </a> </li>
+                              <li> <Link href="#"> Domain &amp; Hosting </Link> </li>
                            </ul>
                         </li>
-                        <li><a href="/ourwork" className={currentRoute === '/ourwork' ? navbar.active : navbar.nonActive}>our work</a></li>
-                        <li><a href="/blog" className={currentRoute === '/blog' ? navbar.active : navbar.nonActive}>blog</a></li>
-                        <li><a href="/contact" className={currentRoute === '/contact' ? navbar.active : navbar.nonActive}>contact</a></li>
+                        <li><Link href="/ourwork" className={currentRoute === '/ourwork' ? navbar.active : navbar.nonActive}>our work</Link></li>
+                        <li><Link href="/blog" className={currentRoute === '/blog' ? navbar.active : navbar.nonActive}>blog</Link></li>
+                        <li><Link href="/contact" className={currentRoute === '/contact' ? navbar.active : navbar.nonActive}>contact</Link></li>
                         <li><div className={navbar.searchBtn}><FontAwesomeIcon icon={faMagnifyingGlass} /></div></li>
                      </ul>
                   </div>
--- src/Layouts/counter/Counter.jsx
+++ src/Layouts/counter/Counter.jsx
@@ -11,19 +11,18 @@ import { useInView } from 'react-intersection-observer';
 
 const Counter = () => {
     const { ref, inView, entry } = useInView({
-        /* Optional options */
         threshold: 0,
-      });
+    });
     return (
         <>
-            <section id="counter" className={counter.counter_section}>
+            <section id="counter" className={counter.counter_section} inView={inView}>
                 <div className="overlay">
                     <div className="container">
                         <div className={counter.counter_wrapper}>
                             <div className="row">
-                                <div className={`col-md-3 col-sm-6 ${counter.counter_column}`}>
+                                <div className={`col-md-3 col-sm-6 ${counter.counter_column}`} ref={ref}>
                                     <h3>Happy Clients</h3>
-                                    <span>{<AnimatedCounter data={{number: '195', duration: '2', delay: 3000}}/>}</span>
+                                    <span>{<AnimatedCounter data={{number: '195', duration: '2', delay: 3000}} />}</span>
                                     <div className={counter.counter_effects}>
                                         <div className={counter.counter_circle}></div>
                                         <div className={counter.counter_img}><Image src={Happy} alt="img"/></div>
