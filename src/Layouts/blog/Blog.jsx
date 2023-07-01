d1132ac2010b44206f0af4d11c1c06dfc9f2445d
Risad212 <hafez.risad@gmail.com>
2023-07-01 20:37:43 +0600

    blog link done

--- src/Layouts/blog/Blog.jsx
+++ src/Layouts/blog/Blog.jsx
@@ -11,6 +11,7 @@ import blogsmall4 from '../../Media/blog-small-4 (1).jpg';
 import blogsmall5 from '../../Media/blog-small-5.jpg';
 import blogsmall6 from '../../Media/blog-small-6 (1).jpg';
 import blogsmall1 from '../../Media/blog-small-1.jpg';
+import Link from 'next/link';
 
 
 const Blog = () => {
@@ -32,7 +33,7 @@ const Blog = () => {
                                         <h2><a href="#">Top 10 Productivity Extensions for Chrome Web Browser</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at October 28, 2019 </div>
                                         <p class={blog.text}>There are limitless ways to ...</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/chrome_top_ten_extensions_details" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
@@ -50,7 +51,7 @@ const Blog = () => {
                                         <h2><a href="#">Top 5 Best Free Adware Removal Tools For Windows</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at May 14, 2019 </div>
                                         <p class={blog.text}>It's can remove, Spyware, Rootkits, PUPs, Virus, Malware, and ...</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/blog_details_2" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
@@ -68,7 +69,7 @@ const Blog = () => {
                                         <h2><a href="#">BEST FREE MICROSOFT OFFICE ALTERNATIVES</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at May 14, 2019 </div>
                                         <p class={blog.text}>10 Best Free Microsoft Office Alternatives</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/blog_details_3" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
@@ -86,7 +87,7 @@ const Blog = () => {
                                         <h2><a href="#">NEXT GENERATION SMARTPHONE</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at March 30, 2019 </div>
                                         <p class={blog.text}>Behold the wonder of the Huawei Mate X</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/blog_details_4" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
@@ -104,7 +105,7 @@ const Blog = () => {
                                         <h2><a href="#">THE 5 MOST IMPORTANT SEO TIPS YOU NEED TO KNOW-NEIL PATEL</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at March 25, 2019 </div>
                                         <p class={blog.text}>Important SEO Tips</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/blog_details_5" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
@@ -122,7 +123,7 @@ const Blog = () => {
                                         <h2><a href="#">BEST FILE SHARING WEBSITES TO SHARE LARGE FILE IN ONLINE</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at June 14, 2019 </div>
                                         <p class={blog.text}>Here we have stated 10 best sites that you will</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/blog_details_5" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
@@ -140,7 +141,7 @@ const Blog = () => {
                                         <h2><a href="#">BEST WINDOWS 10 THEMES/SKINS 2019</a></h2>
                                         <div class={blog.info}>By <em><a href="#">Admin</a></em> at March 25, 2019 </div>
                                         <p class={blog.text}>Here are top 20 best HD Windows 10 themes and Skins 2019 that will enhance your Windows 10 look</p>
-                                        <div class="link-btn"><a href="#" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></a></div>
+                                        <div class="link-btn"><Link href="blog/blog_details_6" class={`${blog.primary_btn} ${blog.hvr_bounce_to_left}`}><span class={blog.btn_text}> MORE </span><span class={blog.icon}><FontAwesomeIcon icon={faArrowRightLong} /></span></Link></div>
                                     </div>
                                 </div>
                             </article>
